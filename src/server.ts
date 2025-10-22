371b520f63cddf5bd47e9446ccbb63f26bb31b47
Haler0708 <haler0708@gmail.com>
2025-10-22 23:17:00 +0200

    Setup ejs

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import ticketsRouter from "./routes/tickets";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(cors());
 app.use(express.json());
