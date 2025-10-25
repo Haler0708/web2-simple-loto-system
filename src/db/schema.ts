a37bd50d1446dc23aa907b8c642b829a31751c63
Haler0708 <haler0708@gmail.com>
2025-10-25 23:37:36 +0200

    Update table names for naming case consistency

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,7 +2,7 @@ import { pgTable, integer, timestamp, text, uuid } from "drizzle-orm/pg-core";
 
 export const tickets = pgTable("tickets", {
   uuid: uuid("uuid").primaryKey().defaultRandom(),
-  idNumber: text("idNumber").notNull(), // OIB ili Broj putovnice
+  idNumber: text("idNumber").notNull(), // broj osoblne ili putovnice
   numbers: integer("numbers").array().notNull(),
-  createdAt: timestamp("created_at").defaultNow().notNull(),
+  createdAt: timestamp("createdAt").defaultNow().notNull(),
 });
