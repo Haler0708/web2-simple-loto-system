3887ea43fdb76d9c35e8269aff40946b6dbb5fd2
Haler0708 <haler0708@gmail.com>
2025-10-26 14:08:29 +0100

    Fix db config to work with renders postgres instance

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
+    ssl: { rejectUnauthorized: false },
   },
   migrations: {
     schema: "public",
