48e81a328385eb054385cdd40af88f05b50343cf
Haler0708 <haler0708@gmail.com>
2025-10-26 15:13:31 +0100

    Fix redirects after login and logout

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,14 +1,17 @@
 import { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const signUp = async (req: Request, res: Response): Promise<void> => {
   res.oidc.login({
-    returnTo: "https://localhost:8443/",
+    returnTo: process.env.BASE_URL,
     authorizationParams: { screen_hint: "signup" },
   });
 };
 
 export const logout = async (req: Request, res: Response): Promise<void> => {
   res.oidc.logout({
-    returnTo: "https://localhost:8443/",
+    returnTo: process.env.BASE_URL,
   });
 };
