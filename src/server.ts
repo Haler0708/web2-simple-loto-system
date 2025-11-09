beaadb633b9d0a5aa0af98b2abc65c03f07c76d0
Haler0708 <haler0708@gmail.com>
2025-11-09 12:12:04 +0100

    Set apps trust proxy to true so the real ip from which the req came from i s logged on render and not ::1

--- src/server.ts
+++ src/server.ts
@@ -19,6 +19,7 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.set("trust proxy", true);
 
 app.use(cors());
 app.use(express.json());
